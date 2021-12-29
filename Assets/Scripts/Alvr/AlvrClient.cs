6487efee04290bd06141e5b1551d4df7f0e8a829
“nosix” <nosix@users.noreply.github.com>
2021-12-29 11:12:41 +0900

    Add fullScreen setting to StreamTexture

--- Assets/Scripts/Alvr/AlvrClient.cs
+++ Assets/Scripts/Alvr/AlvrClient.cs
@@ -10,8 +10,11 @@ namespace Alvr
         [SerializeField] private int recommendedEyeWidth = 1920;
         [SerializeField] private int recommendedEyeHeight = 1080;
         [Range(0.1f, 1.0f)] [SerializeField] private float eyeSizeRatio = 1.0f;
-        [SerializeField] private float[] availableRefreshRates = {60f};
+        [SerializeField] private float[] availableRefreshRates = { 60f };
         [SerializeField] private float preferredRefreshRate = 60f;
+
+        public int EyeWidth => (int) (recommendedEyeWidth * eyeSizeRatio);
+        public int EyeHeight => (int) (recommendedEyeHeight * eyeSizeRatio);
 
         private AndroidJavaObject _androidPlugInInstance;
 
@@ -62,8 +65,8 @@ namespace Alvr
             return new DeviceSettings
             {
                 name = deviceName,
-                recommendedEyeWidth = (int)(recommendedEyeWidth * eyeSizeRatio),
-                recommendedEyeHeight = (int)(recommendedEyeHeight * eyeSizeRatio),
+                recommendedEyeWidth = EyeWidth,
+                recommendedEyeHeight = EyeHeight,
                 availableRefreshRates = availableRefreshRates,
                 availableRefreshRatesLen = availableRefreshRates.Length,
                 preferredRefreshRate = preferredRefreshRate
--- Assets/Scripts/Alvr/AlvrStreamTexture.cs
+++ Assets/Scripts/Alvr/AlvrStreamTexture.cs
@@ -5,15 +5,36 @@ namespace Alvr
 {
     public class AlvrStreamTexture : MonoBehaviour
     {
+        [Tooltip("'width' and 'height' are changed to Screen size.")]
+        public bool fullScreen;
+
         public int width = 3840;
         public int height = 1080;
 
         [SerializeField] private RawImage[] outputImages;
         [SerializeField] private AlvrClient alvrClient;
 
+        private void Awake()
+        {
+            if (fullScreen) UpdateToFullScreenSize();
+        }
+
         private void Start()
         {
             alvrClient.AttachTexture(InitializeTexture2D());
+        }
+
+        private void UpdateToFullScreenSize()
+        {
+            var horizontal = Screen.width;
+            var vertical = Screen.height;
+            if (vertical > horizontal)
+            {
+                (vertical, horizontal) = (horizontal, vertical);
+            }
+
+            width = horizontal;
+            height = vertical;
         }
 
         private Texture2D InitializeTexture2D()
@@ -24,6 +45,7 @@ namespace Alvr
                 rawImage.rectTransform.sizeDelta = new Vector2(width, height);
                 rawImage.texture = texture;
             }
+
             return texture;
         }
 
